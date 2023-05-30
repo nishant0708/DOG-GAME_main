e69f68f5e39ad9699ec91ffac7595ef45381f412
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2023-05-30 20:41:24 +0530

    Add files via upload

---
+++ collisionAnimation.js
@@ -0,0 +1,38 @@
+export class CollisionAnimation{
+    constructor(game,x,y){
+        this.game=game;
+        this.image=document.getElementById('collisionAnimation')
+        this.spritewidth=100;
+        this.spriteheight=90;
+        this.sizeModifier=Math.random()+0.5;
+        this.width=this.spritewidth*this.sizeModifier;
+        this.height=this.spriteheight*this.sizeModifier;
+        this.x = x-this.width * 0.5;
+        this.y = y- this.width*0.5;
+        this.frameX=0;
+        this.maxFrame=4;
+        this.markedDorDeletion=false;
+        this.fps=Math.random()*10+5;
+        this.frameInterval=1000/this.fps;
+        this.frameTimer=0;
+    }
+    draw(context){
+        context.drawImage(this.image,this.frameX*this.spritewidth
+        ,0,this.spritewidth,this.spriteheight,this.x,this.y,
+        this.width,this.height);
+    }
+    update(deltaTime){
+        this.x-=this.game.speed;
+        if(this.frameTimer>this.frameInterval){
+        this.frameX++;
+        this.frameTimer=0;
+        }
+        else{
+            this.frameTimer+=deltaTime;
+        }
+        if(this.frameX >this.maxFrame)this.markedDorDeletion=true;
+
+    }
+
+
+} 
